2feac4627266b8a0cd44034560b095b4480ef2cf
Jason Jon Carreos <jasondev06@gmail.com>
2025-02-05 17:30:33 +1100

    refactor(project): tailwind colors config

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,25 +2,57 @@
  * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
+import hexRgb from 'hex-rgb';
 
+const iconLight = '#687076';
+const iconDark = '#9BA1A6';
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
-export const Colors = {
+type Colors = {
+  text: string;
+  background: string;
+  tint: string;
+  icon: string;
+  tabIconDefault: string;
+  tabIconSelected: string;
+};
+
+const mapColors = (colors: Colors) =>
+  Object.assign(
+    {},
+    ...Object.entries(colors).map(([key, value]) => {
+      const rgb = hexRgb(value);
+
+      return { ['--' + key]: `${rgb.red} ${rgb.green} ${rgb.blue}` };
+    })
+  );
+
+export const AppColors: { light: Colors; dark: Colors } = {
   light: {
     text: '#11181C',
     background: '#fff',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: iconLight,
+    tabIconDefault: iconLight,
     tabIconSelected: tintColorLight,
   },
   dark: {
     text: '#ECEDEE',
     background: '#151718',
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: iconDark,
+    tabIconDefault: iconDark,
     tabIconSelected: tintColorDark,
   },
+};
+
+export const ColorsToConfig = Object.assign(
+  {},
+  ...Object.entries(AppColors.light).map(([key]) => ({ [key]: `rgb(var(--${key}))` }))
+);
+
+export const ColorsToPlugin = {
+  ':root': mapColors(AppColors.light),
+  '.dark:root': mapColors(AppColors.dark),
 };

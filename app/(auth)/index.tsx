9c0b70866ad315ddcda8c8b5633cf51ff68ee6e4
Jason Jon Carreos <jasondev06@gmail.com>
2025-02-10 20:13:40 +1100

    refactor(project): removed unused components

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,10 +1,15 @@
+import { Link } from 'expo-router';
+
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
     <ThemedView className="flex-1 justify-center items-center">
-      <ThemedText>Hey! </ThemedText>
+      <ThemedText>Hey!</ThemedText>
+      <Link className="p-3 mt-6 bg-slate-500" replace href="/(tabs)">
+        <ThemedText>To Tabs</ThemedText>
+      </Link>
     </ThemedView>
   );
 }
